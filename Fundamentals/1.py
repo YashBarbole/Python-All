05d6d0edf95e5871dbc116928464cd5507395af6
YashBarbole <yashbarbole7110@gmail.com>
2026-07-05 13:48:11 +0530

    fundas done

---
+++ Fundamentals/1.py
@@ -0,0 +1,59 @@
+# # cross platform ,high level
+# # python intepreter
+
+#
+
+
+# print("hello yash")
+
+# x = 1
+
+# unit_price = 3
+
+
+# variable is a label to a memo location
+
+students = 1000
+print(students)
+
+
+# primitive - booleans numbers strings
+
+msg = """i am yashi
+i am cool
+okkkk"""
+print(len(msg))
+
+print(msg[0])
+
+
+print(msg[-1])
+
+print(msg[0:3])
+print(msg[0:])
+print(msg[:10])
+print(msg[:])
+
+
+# formatted strings
+
+first = "yash"
+last = "barbole"
+
+full = first + last
+full2 = f"{first} {last}"
+print(full)
+print(full2)
+
+
+# string methods
+
+course = "    python programming"
+
+print(course.upper())
+print(course.title())
+print(course.strip())
+# etc etc many string methods 
+
+#working with numbers
+
