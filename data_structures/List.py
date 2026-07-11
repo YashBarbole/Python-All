ca2197a1d608025b42a0aa11c6dd69ed641cd5af
YashBarbole <yashbarbole7110@gmail.com>
2026-07-11 13:15:20 +0530

    fundas done

---
+++ data_structures/List.py
@@ -0,0 +1,17 @@
+my_list = [1, 2, "yash"]
+
+print(my_list)
+
+
+print(my_list[-3:])
+print(my_list[3:])
+
+# what it does is
+print(my_list[len(my_list) - 3 : len(my_list)])
+# i want more ex here
+
+print(my_list[len(my_list) -1 : len(my_list)])
+
+
+#total length - something and print from that to end
+
