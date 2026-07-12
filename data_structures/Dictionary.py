09553bd23982f63f7c9a01308b3b439d2a9b8246
YashBarbole <yashbarbole7110@gmail.com>
2026-07-12 22:32:50 +0530

    ds started

---
+++ data_structures/Dictionary.py
@@ -0,0 +1,13 @@
+# helps add context to our values
+# almost similar to list
+
+dict = {"x": 1, "y": 2, "z": 4}
+print(dict)
+
+dict["x"] = 11
+print(dict)
+
+
+print(dict.keys())
+print(dict.values())
+print(dict.items())
